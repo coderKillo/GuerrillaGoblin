51904d0e37d555b009a585384c396ceaecef40b8
coderKillo <coderKillo@users.noreply.github.com>
2025-06-10 22:47:17 +0200

    player faces mouse

--- game/entities/components/animation_component.gd
+++ game/entities/components/animation_component.gd
@@ -1,17 +1,30 @@
 extends Node
 
-@export var _animation : AnimatedSprite2D
+@export var _animation: AnimatedSprite2D
 
-@onready var _movement_component : MovementComponent =  $"../MovementComponent"
-@onready var _facing_component : FacingComponent =  $"../FacingComponent"
+@onready var _movement_component: MovementComponent = $"../MovementComponent"
+
 
 func _ready():
 	assert(_movement_component)
 	assert(_animation)
+
 
 func _process(_delta):
 	if _movement_component.is_moving():
 		_animation.play("move")
 	else:
 		_animation.play("idle")
-	
+
+	_flip_sprite()
+
+
+func _flip_sprite():
+	var mouse_position = _animation.get_global_mouse_position()
+	var direction = _animation.global_position.direction_to(mouse_position)
+	var ortho_angle = rad_to_deg(direction.orthogonal().angle())
+
+	if ortho_angle > 0:
+		_animation.flip_h = true
+	else:
+		_animation.flip_h = false
--- game/entities/components/facing_component.gd
+++
@@ -1,5 +0,0 @@
-class_name FacingComponent
-extends Node
-
-
-
