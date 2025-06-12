9e4c74126feb32ea05aa27af0b6b5bb60d79f5ae
coderKillo <coderKillo@users.noreply.github.com>
2025-06-12 22:12:01 +0200

    add settings menu

--- game/menus/main/main_menu.gd
+++ game/menus/main/main_menu.gd
@@ -1,17 +1,28 @@
 extends Control
 
-@onready var _play_button: Button = %Play
-@onready var _exit_button: Button = %Exit
+const MAIN_POSITION := Vector2(640, 360)
+const SETTINGS_POSITION := Vector2(1920, 360)
 
 
 func _ready():
-	_play_button.pressed.connect(_on_play_button_pressed)
-	_exit_button.pressed.connect(_on_exit_button_pressed)
+	%Play.pressed.connect(_on_play_button_pressed)
+	%Setting.pressed.connect(_on_setting_button_pressed)
+	%Exit.pressed.connect(_on_exit_button_pressed)
+
+	%Back.pressed.connect(_on_back_button_pressed)
 
 
 func _on_play_button_pressed():
 	SceneChanger.change_scene(Resources.select_level_scene)
 
 
+func _on_setting_button_pressed():
+	$Camera2D.global_position = SETTINGS_POSITION
+
+
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+
+func _on_back_button_pressed():
+	$Camera2D.global_position = MAIN_POSITION
